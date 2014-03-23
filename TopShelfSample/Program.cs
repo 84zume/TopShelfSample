309ab3257bd12eb253a757d57d96f2b81bdd4635
84zume <hideki.hashizume@gmail.com>
2014-03-23 10:28:23 +0900

    基本的な構造を作成

--- TopShelfSample/Program.cs
+++ TopShelfSample/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Topshelf;
 
 namespace TopShelfSample
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            HostFactory.Run(x =>
+            {
+                x.Service<Beat>(s =>
+                {
+                    s.ConstructUsing(name => new Beat());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription("Just beating program");
+                x.SetDisplayName("BeatingProgram");
+                x.SetServiceName("BeatingService");
+            });
         }
     }
 }
